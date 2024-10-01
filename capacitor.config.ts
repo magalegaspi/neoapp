157022d4259ae42a1b28ee35781bdd52a36f7aa6
magalegaspi <magalegaspi@hotmail.com>
2024-10-01 19:14:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'neoapp',
+  webDir: 'www'
+};
+
+export default config;
