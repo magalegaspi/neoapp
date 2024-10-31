eaf3a29e504b5c73b3a805241a78621dff6cea7d
MathiasHuelmo <100722974+MathiasHuelmo@users.noreply.github.com>
2024-10-31 15:26:45 -0300

    Version final, API Firebase fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAyUypqcVQBqZFGoLhbyCyEQh5poYjBkIQ",
+    authDomain: "neoapp-2722c.firebaseapp.com",
+    projectId: "neoapp-2722c",
+    storageBucket: "neoapp-2722c.appspot.com",
+    messagingSenderId: "628163520860",
+    appId: "1:628163520860:web:fc9fb0372cb2236fb23999",
+    measurementId: "G-C9N2KDWQKC"
+  }
 };
